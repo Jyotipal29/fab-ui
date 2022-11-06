ab864104cbd6dea9f0e1daf17e676393c44bd371
jyoti pal <paljyoti0129@gmail.com>
2022-11-07 01:53:09 +0530

    fix: scroll fixed

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const modal = document.getElementById("modal");
 const showModel = document.getElementById("op-mod");
 const hideModel = document.getElementById("cl-mod");
 
+const links = document.querySelectorAll(".scroll_to");
+
+links.forEach((item) => {
+  item.addEventListener("click", () => {
+    const el = document.getElementById(item.getAttribute("data-link"));
+    el.scrollIntoView({ behavior: "smooth", block: "center" });
+  });
+});
+
 showModel.addEventListener("click", () => {
   modal.classList.remove("hide");
 });
